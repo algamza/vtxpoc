3933bc20194e340e4ec12f37070dd62b17613581
김권식(Kwon-Sik Kim) <kskim2@humaxdigital.com>
2020-08-07 11:07:04 +0900

    BHMC-0, changed file path

--- app/src/main/java/com/humaxdigital/automotive/v2xpoc/presentation/v2x/ImageBinding.kt
+++ app/src/main/java/com/humaxdigital/automotive/v2xpoc/presentation/v2x/ImageBinding.kt
@@ -1,4 +1,4 @@
-package com.humaxdigital.automotive.v2xpoc.presentation.util
+package com.humaxdigital.automotive.v2xpoc.presentation.v2x
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
